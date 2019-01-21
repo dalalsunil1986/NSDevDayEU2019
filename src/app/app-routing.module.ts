90223ef8e750e3c5fa480e15a55442e5032c0560
Rowdy Rabouw <rowdy@double-r.nl>
2019-01-21 13:31:27 +0100

    Create home and speakers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
-    { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
-    { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
-    { path: "featured", loadChildren: "~/app/featured/featured.module#FeaturedModule" },
-    { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" }
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
+  { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
+  { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
+  { path: "speakers", loadChildren: "~/app/speakers/speakers.module#SpeakersModule" },
+  { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" }
 ];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
+  imports: [NativeScriptRouterModule.forRoot(routes)],
+  exports: [NativeScriptRouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from "@angular/core";
-import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { RouterExtensions } from "nativescript-angular/router";
+import { Page } from "ui/page";
 import * as app from "tns-core-modules/application";
 
 @Component({
-    selector: "Home",
-    moduleId: module.id,
-    templateUrl: "./home.component.html"
+  selector: "Home",
+  moduleId: module.id,
+  templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+  constructor(private page: Page, private routerExtensions: RouterExtensions) {}
 
-    constructor() {
-        // Use the component constructor to inject providers.
-    }
+  ngOnInit(): void {
+    this.page.actionBarHidden = true;
+  }
 
-    ngOnInit(): void {
-        // Init your component properties here.
-    }
-
-    onDrawerButtonTap(): void {
-        const sideDrawer = <RadSideDrawer>app.getRootView();
-        sideDrawer.showDrawer();
-    }
+  onNavItemTap(navItemRoute: string): void {
+    this.routerExtensions.navigate([navItemRoute], {
+      transition: {
+        name: "fade"
+      }
+    });
+  }
 }
