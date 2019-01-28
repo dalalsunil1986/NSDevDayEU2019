e11eaf50860594743e3f5294b2f3b94835d61bfa
Rowdy Rabouw <rowdy@double-r.nl>
2019-01-28 12:17:56 +0100

    Add speaker service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { NativeScriptSvgModule } from "nativescript-svg/angular";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
+import { SpeakerService } from "./services/speaker.service";
+
 @NgModule({
   bootstrap: [AppComponent],
   imports: [AppRoutingModule, NativeScriptModule, NativeScriptSvgModule],
   declarations: [AppComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [SpeakerService]
 })
 export class AppModule {}
