6e9a38db9269f78a0eb54c97c2d8aa46311bc455
Rowdy Rabouw <rowdy@double-r.nl>
2019-02-07 20:24:23 +0100

    Add location service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,13 @@ import { AppComponent } from "./app.component";
 import { SpeakerService } from "./services/speaker.service";
 import { ScheduleService } from "./services/schedule.service";
 import { AboutService } from "./services/about.service";
+import { LocationService } from "./services/location.service";
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [AppRoutingModule, HttpClientModule, NativeScriptModule, NativeScriptHttpClientModule, NativeScriptSvgModule],
   declarations: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
-  providers: [SpeakerService, ScheduleService, AboutService]
+  providers: [SpeakerService, ScheduleService, AboutService, LocationService]
 })
 export class AppModule {}
